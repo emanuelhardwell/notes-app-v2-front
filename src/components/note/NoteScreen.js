85d344cae035aa6b827291eb1fa6f917c974cef3
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-23 17:18:21 -0500

    Se diseño la estructura de nuestro proyecto y ademas se creo el Navbar

---
+++ src/components/note/NoteScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navbar } from "../ui/Navbar";
+
+export const NoteScreen = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>NoteScreen</h1>
+    </div>
+  );
+};
---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            Notes App
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="#">
+                  Features
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="#">
+                  Salir
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
