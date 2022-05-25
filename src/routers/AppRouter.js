d9f88403f18e2a3882436c2a0374cddbbf625d68
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-25 15:20:04 -0500

    [Router] se implementaron los compoentes PrivateRoute y PublicRoute

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,50 @@
-import React from "react";
-import { Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { Switch } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { startAuthCheckingFinish } from "../actions/authActions";
 import { LoginScreen } from "../components/auth/LoginScreen";
 import { RegisterScreen } from "../components/auth/RegisterScreen";
 import { NoteScreen } from "../components/note/NoteScreen";
+import { PrivateRouter } from "./PrivateRouter";
+import { PublicRouter } from "./PublicRouter";
 
 export const AppRouter = () => {
+  const dispatch = useDispatch();
+  const { checking, uid } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(startAuthCheckingFinish());
+  }, [dispatch]);
+
+  if (checking) {
+    return <h5> LOADING .................... </h5>;
+  }
+
   return (
     <>
       <Router>
         <div>
           <Switch>
-            <Route exact path="/" component={NoteScreen} />
-            <Route exact path="/login" component={LoginScreen} />
-            <Route exact path="/register" component={RegisterScreen} />
+            <PrivateRouter
+              exact
+              path="/"
+              component={NoteScreen}
+              isAuthenticated={!!uid}
+            />
+            <PublicRouter
+              exact
+              path="/login"
+              component={LoginScreen}
+              isAuthenticated={!!uid}
+            />
+            <PublicRouter
+              exact
+              path="/register"
+              component={RegisterScreen}
+              isAuthenticated={!!uid}
+            />
             <Redirect to="/" />
           </Switch>
         </div>
