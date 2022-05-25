5d52f6e93e9e3dabe947f8f32e8eed457ea67b78
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-24 23:30:57 -0500

    [Diseño] se diseño el Login y Register

---
+++ src/components/auth/LoginScreen.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+
+const initialState = {
+  lemail: "juan@gmail.com",
+  lpassword: "123456",
+};
+
+export const LoginScreen = () => {
+  const [formLoginValues, handleLoginInputChange] = useForm(initialState);
+
+  const { lemail, lpassword } = formLoginValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(formLoginValues);
+  };
+
+  return (
+    <>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6 p-5 mx-auto">
+            <div className="card">
+              <div className="card-body">
+                <h4 className="mb-4 text-center">Iniciar sesión</h4>
+                <form onSubmit={handleLogin}>
+                  <div className="mb-3 input-group">
+                    <span className="input-group-text" id="basic-addon1">
+                      <i className="fa-solid fa-user"></i>
+                    </span>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Email"
+                      name="lemail"
+                      value={lemail}
+                      onChange={handleLoginInputChange}
+                    />
+                  </div>
+
+                  <div className="mb-3 input-group">
+                    <span className="input-group-text" id="basic-addon1">
+                      <i className="fa-solid fa-lock"></i>
+                    </span>
+                    <input
+                      type="password"
+                      className="form-control"
+                      placeholder="Password"
+                      name="lpassword"
+                      value={lpassword}
+                      onChange={handleLoginInputChange}
+                    />
+                  </div>
+
+                  <div className="mb-3 d-grid gap-2">
+                    <button type="submit" className="btn btn-primary">
+                      Login <span></span>
+                      <i className="fa-solid fa-right-to-bracket"></i>
+                    </button>
+                  </div>
+                </form>
+
+                <div className="mb-3">
+                  <Link to="#"> ¿Olvidaste tu contraseña? </Link>
+                </div>
+              </div>
+
+              <div className="card-footer">
+                <p className="pt-2 text-center">
+                  ¿No tienes una cuenta?
+                  <span> </span>
+                  <Link to="/register">¡Regístrese y comience! </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
